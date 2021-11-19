2426fa4caf7bef823483bfc959d9e63bc6227fc6
fquijada <fquijada@centribal.com>
2021-11-19 15:01:00 -0300

    feature: first version with pagination

---
+++ src/components/Cards.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/Cards.css';
+
+export const Cards = React.forwardRef(({player}, ref) => {
+    return (
+        <div className='player-card' ref={ref}>
+            <section className='top'>
+                <section className='image-name-tagline'>
+                    <img src={player.avatar} alt={player.nickname}/>
+                    <section className='right-side'>
+                        <p className='player-name'>{player.nickname}</p>
+                        <p className='player-info'>
+                            <i>{player.status}</i>
+                            <i>{player.balance}</i>
+                        </p>
+                    </section>
+                </section>
+            </section>
+        </div>
+    );
+});
