c5443bde5da64bc5f96be412ef18ea17fd9b2bbc
fquijada <fquijada@centribal.com>
2021-11-20 19:20:54 -0300

    refactor: component page Home separate presentation to logic

--- src/pages/players/Home/Home.test.js
+++ src/pages/players/Home/Home.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import {render} from '@testing-library/react';
-import {Home} from './Home';
+import {HomeView} from './HomeView';
 
 test('renders content', () => {
-    const component = render(<Home />);
+    const component = render(<HomeView />);
     component.getByText('List of Players');
 });
---
+++ src/pages/players/Home/HomeContainer.jsx
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {HomeView} from "./HomeView";
+
+export const HomeContainer = () => {
+    const [cards, setCards] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState();
+    const [searchPlayers, setSearchPlayers] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        fetchPlayersData();
+    }, [currentPage, searchPlayers]);
+
+    const fetchPlayersData = async () => {
+        const quantityPages = 20;
+        setLoading(true);
+        await axios.get(
+            `${process.env.REACT_APP_API_URL}players/?search=${searchPlayers}&page=${currentPage}&limit=${quantityPages}`
+        ).then(response => {
+            setTotalPages(calculateTotalPages(response.data.total, quantityPages));
+            setCards(response.data.data);
+            setLoading(false);
+        }).catch(error => {
+            setLoading(false);
+        });
+    };
+
+    const calculateTotalPages = (total, quantity) => {
+        let numberPages = Math.trunc(total / quantity);
+        return numberPages <= 0 ? 1 : numberPages;
+    }
+
+    const handlePageChange = (e) => {
+        setCurrentPage(e.target.innerText);
+    };
+
+    return <HomeView
+        setSearchPlayers={setSearchPlayers}
+        loading={loading}
+        handlePageChange={handlePageChange}
+        totalPages={totalPages}
+        currentPage={currentPage}
+        cards={cards}
+    />
+};
+
+
+
+
