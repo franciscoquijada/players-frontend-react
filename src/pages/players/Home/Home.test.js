db06ff03b8812592caa8bfefddbc1be0049197c8
fquijada <fquijada@centribal.com>
2021-11-20 18:27:48 -0300

    test: add first test home

--- src/pages/players/Home/Home.test.js
+++ src/pages/players/Home/Home.test.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import {render} from '@testing-library/react';
-import Home from './Home';
-import {getByText} from "@testing-library/dom";
+import {Home} from './Home';
 
 test('renders content', () => {
     const component = render(<Home />);
-
     component.getByText('List of Players');
-})
+});
