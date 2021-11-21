fa0104f648b8b91e1bc9ddb63b82260b44525b8c
fquijada <fquijada@centribal.com>
2021-11-21 08:52:33 -0300

    test: add e2e test to show message player not found

--- cypress/integration/players/players_page.spec.js
+++ cypress/integration/players/players_page.spec.js
@@ -6,12 +6,20 @@ describe('Players Page', () => {
     it('Init page can be opened', () => {
         cy.contains('List of Players');
         cy.get('input').type('1');
+        cy.get('.card').should('have.length.greaterThan', 2);
     });
 
     it('Should be can search players by number', () => {
         cy.get('input').type('1');
         cy.contains('Searching Players');
         const expectedCount = 1;
+        cy.get('.card').should('have.length', expectedCount);
+    });
+
+    it('Should be can show message player not found', () => {
+        cy.get('input').type('0');
+        cy.contains('No players were found');
+        const expectedCount = 0;
         cy.get('.card').should('have.length', expectedCount);
     });
 
