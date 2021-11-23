767f1648fa1add3644f598ae68b62763e5c0825e
fquijada <fquijada@centribal.com>
2021-11-22 22:14:45 -0300

    fix: better loading

--- src/components/shared/Loading/Loading.js
+++ src/components/shared/Loading/Loading.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { LinearProgress } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import { CircularProgress } from '@material-ui/core';
 
 export const Loading = ({ message }) => {
   return (
     <>
       <h2>{message}</h2>
-      <LinearProgress size="20%" />
+      <CircularProgress color="primary" size="10%" />
     </>
   );
 };
